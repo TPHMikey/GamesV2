ced2a6a9a556932c6ab78fd558f61b3ebece912e
Mikeyboy6734 <Mikeygett@outlook.com>
2025-11-03 21:50:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
 const games = [
-  {
-    name: "Flappy Bird",
-    thumbnail: "assets/images/flappy.png",
-    path: "games/flappy/index.html",
-    category: "puzzle"
-  },
-  {
-    name: "2D Minecraft",
-    thumbnail: "assets/images/2DMC.png",
-    path: "games/2D%20Minecraft/2D%20Minecraft.html",
-    category: "adventure"
-  },
-  {
-    name: "PacMan",
-    thumbnail: "assets/images/PM.png",
-    path: "games/PacMan/index.html",
-    category: "adventure"
-  },
-  {
-    name: "Geometry Dash",
-    thumbnail: "assets/images/GM.png",
-    path: "games/Geometry%20Dash/index.html",
-    category: "adventure"
-  },
-  {
+  {,
     name: "Five Nights At Freddys 1",
     thumbnail: "assets/images/FNAF1.png",
     url: "https://mikeyboy6734.github.io/FNAF1-Web",
